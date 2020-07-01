c67fa0fddbcce74f8d52f44b7a6e057e853dba60
kirti <kirtigupta9628@gmail.com>
2020-07-01 23:22:02 +0530

    Namespace structure added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Konva from "konva";
+import {Namespace} from "./shapes/Namespace";
 
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -11,7 +12,6 @@ var stage = new Konva.Stage({
 var layer = new Konva.Layer();
 stage.add(layer);
 
-// what is url of dragging element?
 var itemURL = '';
 if (!document) throw new Error("Not doc env");
 
@@ -26,20 +26,32 @@ con.addEventListener('dragover', function (e) {
 });
 
 con.addEventListener('drop', function (e) {
+	let resource: any;
 	e.preventDefault();
-	// now we need to find pointer position
-	// we can't use stage.getPointerPosition() here, because that event
-	// is not registered by Konva.Stage
-	// we can register it manually:
 	stage.setPointersPositions(e);
 
 	Konva.Image.fromURL(itemURL, function (image: Konva.Image) {
-		layer.add(image);
-
+		image.setAttrs({
+			x: 100,
+			y: 0,
+			offsetX: 50,
+			offsetY: 30,
+			scaleX: 0.8,
+			scaleY: 0.8,
+		  });
+			resource = new Namespace({
+				width: 800,
+				height: 500,
+				stroke: 'black',
+				strokeWidth: 2,
+				dashEnabled: true,
+				dash: ([2,4])
+			}, image);
+		layer.add(resource.Shape);
+		resource.Shape.position(stage.getPointerPosition());
 		//@ts-ignore
-		image.position(stage.getPointerPosition());
-		image.draggable(true);
+		resource.Shape.draggable(true);
 
-		layer.draw();
+		layer.batchDraw();
 	});
 });
